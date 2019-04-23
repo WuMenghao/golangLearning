6216601a9c7f816b18091b4e6e8c8ffd7fd1907e
WuMenghao <tm_wmh@foxmail.com>
2019-04-23 22:12:50 +0800

    2019/04/23

---
+++ statement.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+/**
+一、条件语句
+1.if条件语句不需要括号
+2.if条件里可以赋值,if条件里的变量作用域仅在if语句里
+
+二、switch case 语句
+1.switch case 自动会break ,除非使用fallthrough
+2.switch 后面可以没有表达式
+
+三、for 语句
+1.for的条件不需要括号
+2.for的条件里可以省略初始条件,结束条件,递增表达式
+*/
+
+func ifStatement() {
+	const fileName = "abc.txt"
+	if contents, err := ioutil.ReadFile(fileName); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s \n", contents)
+	}
+}
+
+func switchStatement(score int) string {
+	g := ""
+	switch {
+	case score < 60:
+		g = "D"
+	case score < 80 && score >= 60:
+		g = "C"
+	case score < 90 && score >= 80:
+		g = "B"
+	case score <= 100 && score >= 90:
+		g = "A"
+	case score < 0 || score > 100:
+		panic(fmt.Sprintf("Wrong score: %d", score))
+	}
+	return g
+}
+
+func forLoop(n int) string {
+	//convertToBein
+	result := ""
+	if n == 0 {
+		return "0"
+	}
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+func printFile(fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	//while类似
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func forever() {
+	for {
+		println("abc")
+	}
+}
+
+func main() {
+	ifStatement()
+
+	fmt.Println(
+		switchStatement(100),
+		switchStatement(59),
+		switchStatement(89),
+		switchStatement(10),
+	)
+
+	fmt.Println(
+		forLoop(5),
+		forLoop(13),
+		forLoop(12334515),
+		forLoop(0),
+	)
+
+	printFile("abc.txt")
+	//forever()
+}
--- variable.go
+++ variable.go
@@ -7,19 +7,47 @@ import (
 )
 
 /**
+·变量类型写在变量名之后
+·编译器可以推测变量类型
+·没有char,只有rune(4byte)
+·原生支持复数类型
+
+
 一、变量定义
 1.var 关键字用于定义变量
 2.var 关键词可以连续定义多个变量
 3.var 关键词可以用 := 代替定义局部变量
 4.var 关键词可以在包内方法外使用，:=不可以，在包内定义的变量只能在包内使用
+
 二、变量类型
 1.bool,string
 2.(u)int ,(u)int8,(u)int16,(u)int32,(u)int64,uintptr
 3.byte,rune(字符，长度为32位，4字节)
 4.float32,float64,complex64,complex128(复数)
+
 三、类型转换
 1.int()
 2.类型转换是强制的
+
+四、常量
+1.const 关键字用于定义常量
+2.const 数值可以不规定类型作为各种类型使用 const a,b = 3 ,4
+
+五、枚举
+1.普通枚举类型
+	const (
+		java = 0
+		cpp = 1
+		python = 2
+		golang = 3
+	)
+2.自增值枚举类型
+	const (
+		java = iota //自增
+		cpp
+		python
+		golang
+	)
 */
 func variableZeroValue() {
 	var a int
@@ -66,6 +94,42 @@ func triangle() {
 	fmt.Println(c)
 }
 
+func consts() {
+	const fileName = "abc.txt"
+	const a, b = 3, 4
+	var c int
+	c = int(math.Sqrt(a*a + b*b))
+
+	fmt.Println(fileName, c)
+}
+
+func enums() {
+	//golang 没有枚举关键词，直接使用const定义一组常量
+	const (
+		//java = 0
+		//cpp = 1
+		//python = 2
+		//golang = 3
+
+		java = iota //自增
+		cpp
+		python
+		golang
+	)
+	fmt.Println(java, cpp, python, golang)
+
+	//iota 可用于计算
+	const (
+		b = 1 << (10 * iota)
+		kb
+		mb
+		gb
+		tb
+		pb
+	)
+	fmt.Println(b, kb, mb, gb, pb, tb, pb)
+}
+
 func main() {
 	variableZeroValue()
 	variableInitValue()
@@ -75,4 +139,7 @@ func main() {
 
 	euler()
 	triangle()
+
+	consts()
+	enums()
 }
