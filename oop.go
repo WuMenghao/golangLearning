51ceb56d8ea40f9a14aa75e2f9b67473441c9fb6
WuMenghao <33742731+WuMenghao@users.noreply.github.com>
2019-05-09 13:34:42 +0800

    oop.go

--- oop.go
+++ oop.go
@@ -10,6 +10,12 @@ import "fmt"
 
 二、工厂函数
 	使用自定义工厂函数，返回值为局部变量的地址
+
+三、方法接收者
+	1.golang 的 struct中无法定义方法
+	2.golang 中 func的定义可以指定接收者
+	3.只有使用指针作为方法的接收者才可以改变结构内容
+	4.nil指针也可以调用方法
 */
 
 //TreeNode 类型
@@ -18,13 +24,31 @@ type TreeNode struct {
 	left, right *TreeNode
 }
 
+//TreeNode 的工厂方法
+func createTreeNode(value int) *TreeNode {
+	return &TreeNode{value: value}
+}
+
+//func 定义 有接收者
 func (node TreeNode) print() {
 	fmt.Println(node.value)
 }
 
-//TreeNode 的工厂方法
-func createTreeNode(value int) *TreeNode {
-	return &TreeNode{value: value}
+func (node *TreeNode) setValue(value int) {
+	if node == nil {
+		fmt.Println("Setting value to nie pointer ignored")
+		return
+	}
+	node.value = value
+}
+
+func (node *TreeNode) traverse() {
+	if node != nil {
+		return
+	}
+	node.left.traverse()
+	node.print()
+	node.right.traverse()
 }
 
 func main() {
@@ -42,7 +66,21 @@ func main() {
 	//使用工厂函数创建对象
 	root.right.left = createTreeNode(2)
 	fmt.Println(root.left)
-
-	root.print()
 	fmt.Println()
+
+	//函数 即可以使用指针调用，也能用对象调用，很方便
+	root.print()
+
+	root.setValue(5)
+	root.print()
+
+	pRoot := &root
+	pRoot.setValue(10)
+	pRoot.print()
+
+	//使用nil指针调用方法
+	var npRoot *TreeNode
+	npRoot.setValue(100) //panic: runtime error: invalid memory address or nil pointer dereference
+
+	root.traverse()
 }
